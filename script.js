820bb96afd0e61cebd3249833de37085a1d3455b
Rodrigo <rodrigo.nunes.trabalho@hotmail.com>
2023-01-28 15:06:18 -0300

    Segundo desafio VNW

---
+++ script.js
@@ -0,0 +1,51 @@
+//PRIMEIRO EXERCÍCIO//
+function um() {
+console.log("Exercício número 1") 
+}
+um()
+//PRIMEIRO EXERCÍCIO//
+
+//-----------------//
+
+//SEGUNDO EXERCÍCIO//
+function dois(nome){
+console.log("Olá, meu nome é"+nome)
+}
+dois(" Rodrigo ")
+//SEGUNDO EXERCÍCIO//
+
+//-----------------//
+
+//TERCEIRO EXERCÍCIO//
+function tres(nome, idade, estiloMusical){
+console.log("Olá, meu nome é"+nome,"tenho"+idade,"e meu gênero musical preferido é"+estiloMusical)
+}
+tres(" Rodrigo,"," 23 anos"," Rap")
+//TERCEIRO EXERCÍCIO//
+
+//-----------------//
+
+//QUARTO EXERCÍCIO//
+function quatro(filme, música){
+console.log("Um dos últimos filmes que assisti foi"+filme,"criado pelo rapper Kid Cudi, cujo também participou e elaborou a trilha sonora. Uma das músicas da trilha se chama"+música,"e tem participação especial do rapper Ty Dolla $ign.")
+}
+quatro(" Entergalactic"," 'Willing To Trust'")
+//QUARTO EXERCÍCIO//
+
+//-----------------//
+
+//QUINTO EXERCÍCIO//
+function cinco(triplo){
+return triplo * 3
+}
+console.log(cinco(5))
+//QUINTO EXERCÍCIO//
+
+//-----------------//
+
+//SEXTO...U EXERCÍCIO//
+let x = 6 > 5
+let y = 6 < 5
+console.log(x)
+console.log(y)
+//SEXTO...U EXERCÍCIO//
